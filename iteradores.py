7b2bd0bfdce2ef0062a6f47ad3bd5badac665dd6
yfsanchez <yfsanchez@gmail.com>
2022-02-13 20:12:50 -0300

    iterador in python : ciclo for con while true

--- iteradores.py
+++ iteradores.py
@@ -1,4 +1,18 @@
+#creando un iterador
+#ejemplo de lo que hace un ciclo for
+def iterar():
+    my_list = [1,2,3,4,56,6]
+    my_iter = iter(my_list)
+
+    while True:
+        try:
+            element = next(my_iter)
+            print(element)
+        except StopIteration:
+            break
+
 def run():
-    print("test")
+    iterar()
+
 if __name__ == '__main__':
     run()

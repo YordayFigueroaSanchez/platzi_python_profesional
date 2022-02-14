1fda30954b66f560d635d190465d47ea4d1e6590
yfsanchez <yfsanchez@gmail.com>
2022-02-14 08:58:09 -0300

    generador - ejemplo de succesion de fibonacci con menos codigo

--- generadores_fibonacci.py
+++ generadores_fibonacci.py
@@ -1,40 +1,30 @@
 import time
 
 def fibonacci_generator():
-    n0 = 0
-    n1 = 1
-    counter = 0
+    n0, n1 = 0, 1
     while True:
-        if counter == 0:
-            counter += 1
-            yield n0
-        elif counter == 1:
-            counter += 1
-            yield n1
-        else:
-            aux = n0 + n1
-            n0, n1 = n1, aux
-            counter += 1
-            yield aux
-def fibonacci_generator(max : int) -> int:
-    n0 = 0
-    n1 = 1
-    counter = 0
-    while n0+n1 <= max:
-        if counter == 0:
-            counter += 1
-            yield n0
-        elif counter == 1:
-            counter += 1
-            yield n1
-        else:
-            aux = n0 + n1
-            n0, n1 = n1, aux
-            counter += 1
-            yield aux
+        yield n0
+        n0, n1 = n1, n0+n1
+
+# def fibonacci_generator(max : int) -> int:
+#     n0 = 0
+#     n1 = 1
+#     counter = 0
+#     while n0+n1 <= max:
+#         if counter == 0:
+#             counter += 1
+#             yield n0
+#         elif counter == 1:
+#             counter += 1
+#             yield n1
+#         else:
+#             aux = n0 + n1
+#             n0, n1 = n1, aux
+#             counter += 1
+#             yield aux
 
 if __name__ == '__main__':
-    fibonacci = fibonacci_generator(50)
+    fibonacci = fibonacci_generator()
     for element in fibonacci:
         print(element)
         time.sleep(1)

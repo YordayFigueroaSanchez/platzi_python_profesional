fc08f635e118ef28a3fba1eab4509bfc979bd304
yfsanchez <yfsanchez@gmail.com>
2022-02-15 23:29:39 -0300

    conjuntos - ejemplo de sets

--- sets.py
+++ sets.py
@@ -12,6 +12,24 @@ def run():
     list_number = [1,2,3,4,5,6,7,3,6,7,8,9]
     list_number = remove_duplicate(list_number)
     print(list_number)
+    
+    #set
+    my_set1 = {3,4,5}
+    my_set2 = {5,6,7}
+    print('#union | ')
+    my_set_union = my_set1 | my_set2
+    print(my_set_union)
+    print('#interseccion & ')
+    my_set_interseccion = my_set1 & my_set2
+    print(my_set_interseccion)
+    print('#diferencia - ')
+    my_set_diferencia = my_set1 - my_set2
+    print(my_set_diferencia)
+    print('#diferencia simetrica ^ ')
+    my_set_diferencia_simetrica = my_set1 ^ my_set2
+    print(my_set_diferencia_simetrica)
+
+
 
 if __name__ == '__main__':
     run()

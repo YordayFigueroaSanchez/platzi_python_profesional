20d62c79a90b4f9a2191913c6e9f5ba0bb1fe505
yfsanchez <yfsanchez@gmail.com>
2022-02-15 23:47:28 -0300

    time zone

--- fechas.py
+++ fechas.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 import datetime
+import pytz
 
 def run():
     my_time = datetime.datetime.now()
@@ -10,6 +12,13 @@ def run():
     print('formateo de fecha')
     my_str = my_time.strftime('%d/%m/%Y')
     print(my_str)
+    #ZONA HORARIA
+    bogota_timezone = pytz.timezone("America/Bogota")
+    bogota_date = datetime.datetime.now(bogota_timezone)
+    print("Bogota : " + bogota_date.strftime("%d/%m/%Y  %H:%M:%S"))
+    mexico_timezone = pytz.timezone("America/Mexico_City")
+    mexico_date = datetime.datetime.now(mexico_timezone)
+    print("Mexico : " + mexico_date.strftime("%d/%m/%Y  %H:%M:%S"))
 
 if __name__ == '__main__':
     run()
